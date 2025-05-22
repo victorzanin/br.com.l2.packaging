9cd0f6e56d5905528187c9738ab201f870d0f7a9
victor.zanin <zanin.victoradriano@gmail.com>
2025-05-22 18:26:14 -0300

    Refactor

---
+++ src/main/java/br/com/l2/packaging/model/Box.java
@@ -0,0 +1,4 @@
+package br.com.l2.packaging.model;
+
+public record Box(String nome, int altura, int largura, int comprimento) {
+}
--- src/main/java/br/com/l2/packaging/service/impl/PackagingServiceImpl.java
+++ src/main/java/br/com/l2/packaging/service/impl/PackagingServiceImpl.java
@@ -6,74 +6,83 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+
+import static java.util.Objects.nonNull;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Service
 @NoArgsConstructor
 public class PackagingServiceImpl implements PackagingService {
 
-    private static final List<int[]> CAIXAS = Arrays.asList(
-            new int[]{30, 40, 80},
-            new int[]{80, 50, 40},
-            new int[]{50, 80, 60}
+    private static final String OBS_NOT_FIT_IN_THE_BOX = "Produto(s) não cabe(m) em nenhuma caixa disponível.";
+    private static final List<Box> BOXES = List.of(
+            new Box("Caixa 1", 30, 40, 80),
+            new Box("Caixa 2", 80, 50, 40),
+            new Box("Caixa 3", 50, 80, 60)
     );
 
     @Override
     public OrderResponse orderPackage(OrderRequest orderRequest) {
         OrderResponse response = new OrderResponse();
-        orderRequest.getPedidos().forEach(
-                order -> {
-                    var boxes = runPackage(order.getProdutos());
-                    OrderDetailsResponse orderDetail = new OrderDetailsResponse();
-                    orderDetail.setPedidoId(order.getPedidoId());
-                    orderDetail.setCaixas(boxes);
-                    response.addPedidosItem(orderDetail);
-                });
+        orderRequest.getPedidos().forEach(order -> {
+            List<ProductBoxResponse> boxes = runPackage(order.getProdutos());
+            var detail = new OrderDetailsResponse();
+            detail.setCaixas(boxes);
+            detail.setPedidoId(order.getPedidoId());
+            response.addPedidosItem(detail);
+        });
         return response;
     }
 
-    public List<ProductBoxResponse> runPackage(List<ProductRequest> products) {
+    private List<ProductBoxResponse> runPackage(List<ProductRequest> products) {
         List<ProductBoxResponse> response = new ArrayList<>();
-        List<ProductRequest> notAllowed = new ArrayList<>(products); // Copia original da lista para evitar problemas na remoção
+        List<ProductRequest> notAllowed = new ArrayList<>(products);
 
-        for (int[] caixa : CAIXAS) {
-            String boxName = "Caixa " + (CAIXAS.indexOf(caixa) + 1);
-            List<String> produtosNaCaixa = new ArrayList<>();
+        for (Box box : BOXES) {
+            var productInBox = notAllowed.stream()
+                    .filter(product -> checkProductMeasurement(product, box))
+                    .map(ProductRequest::getProdutoId)
+                    .toList();
 
-            Iterator<ProductRequest> iterator = notAllowed.iterator();
-            while (iterator.hasNext()) {
-                ProductRequest product = iterator.next();
-                var dimension = product.getDimensoes();
+            notAllowed.removeIf(product -> checkProductMeasurement(product, box));
 
-                if (dimension.getAltura() <= caixa[0] &&
-                        dimension.getLargura() <= caixa[1] &&
-                        dimension.getComprimento() <= caixa[2]) {
-
-                    produtosNaCaixa.add(product.getProdutoId());
-                    iterator.remove();
-                }
-            }
-
-            if (!produtosNaCaixa.isEmpty()) {
-                ProductBoxResponse boxResponse = new ProductBoxResponse();
-                boxResponse.setProdutos(produtosNaCaixa);
-                boxResponse.setCaixaId(boxName);
-                response.add(boxResponse);
+            if (!productInBox.isEmpty()) {
+                response.add(buildProductBoxResponse(box.nome(), productInBox, null));
             }
         }
 
         if (!notAllowed.isEmpty()) {
-            ProductBoxResponse notAllowedBox = new ProductBoxResponse();
-            notAllowedBox.setCaixaId(null);
-            notAllowedBox.setProdutos(
-                    notAllowed.stream().map(ProductRequest::getProdutoId).toList()
+            ProductBoxResponse productBox = buildProductBoxResponse(
+                    null,
+                    notAllowed.stream().map(ProductRequest::getProdutoId).toList(),
+                    OBS_NOT_FIT_IN_THE_BOX
             );
-            notAllowedBox.setObservacao("Produto(s) não cabe(m) em nenhuma caixa disponível.");
-            response.add(notAllowedBox);
+            response.add(productBox);
         }
 
         return response;
+    }
+
+    private ProductBoxResponse buildProductBoxResponse(String boxId, List<String> products, String obs) {
+        ProductBoxResponse productBox = new ProductBoxResponse();
+
+        if (isNotBlank(boxId))
+            productBox.setCaixaId(boxId);
+
+        if (isNotBlank(obs))
+            productBox.setObservacao(obs);
+
+        if (nonNull(products))
+            productBox.setProdutos(products);
+
+        return productBox;
+    }
+
+    private boolean checkProductMeasurement(ProductRequest product, Box box) {
+        var dimension = product.getDimension();
+        return dimension.getAltura() <= box.altura() &&
+                dimension.getLargura() <= box.altura() &&
+                dimension.getComprimento() <= box.comprimento();
     }
 }
